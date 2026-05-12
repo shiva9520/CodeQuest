722cf9a6da1a897e6c79ebf413bc769df63fb5ef
shiva9520 <kashyapshiva073@gmail.com>
2026-05-12 23:29:27 +0530

    create sign in page ui

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SignUpScreen from './src/screens/SignUpScreen'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
+import SignInScreen from './src/screens/SignInScreen'
+import { getAuth, FirebaseAuthTypes } from '@react-native-firebase/auth'
 const App = () => {
+  const [user, setUSer] = useState<FirebaseAuthTypes.User | null>(null);
+  useEffect(()=>{
+    const auth = getAuth();
+    const unsubscribe = auth.onAuthStateChanged(user => setUSer(user));
+    return unsubscribe;
+  })
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <SignUpScreen/>
+      <SignInScreen/>
     </SafeAreaView>
   )
 }
---
+++ src/screens/SignInScreen.tsx
@@ -0,0 +1,140 @@
+import { 
+  getAuth,
+  signInWithEmailAndPassword,
+} from '@react-native-firebase/auth';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const SignInScreen = () => { 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = () => {
+    signInWithEmailAndPassword(getAuth(), email, password)
+      .then(() => {
+        // console.log('User account created & signed in!');
+        Alert.alert('Success', 'User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          Alert.alert('Error', 'That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          Alert.alert('Error', 'That email address is invalid!');
+        }
+
+        Alert.alert('Error', 'Failed to sign in!');
+      });
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.title}>Create Account</Text>
+        <Text style={styles.subtitle}>Sign up to continue</Text>
+
+        {/* Email */}
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          placeholderTextColor="#888"
+        />
+
+        {/* Password */}
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+          placeholderTextColor="#888"
+        />
+
+        {/* Button */}
+        <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+          <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.footerText}>Already have an account? Login</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default SignInScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+
+  card: {
+    backgroundColor: '#fff',
+    padding: 25,
+    borderRadius: 15,
+    elevation: 5,
+  },
+
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#222',
+    textAlign: 'center',
+  },
+
+  subtitle: {
+    fontSize: 14,
+    color: '#666',
+    textAlign: 'center',
+    marginTop: 5,
+    marginBottom: 25,
+  },
+
+  input: {
+    height: 55,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+    fontSize: 16,
+    backgroundColor: '#fafafa',
+    color: '#000',
+  },
+
+  button: {
+    backgroundColor: '#4A90E2',
+    paddingVertical: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+
+  footerText: {
+    textAlign: 'center',
+    marginTop: 20,
+    color: '#666',
+  },
+});
